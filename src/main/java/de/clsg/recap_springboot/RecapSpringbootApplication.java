3f8e7bfd4af315850cc3b70fb191dbcaeae3b90a
ChrisClsg <chris@clsg.de>
2026-03-20 10:33:12 +0100

    Init Spring Boot project
    Java 25
    Dep: Spring Web, Spring Data, Lombok, MongoDB

---
+++ src/main/java/de/clsg/recap_springboot/RecapSpringbootApplication.java
@@ -0,0 +1,13 @@
+package de.clsg.recap_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapSpringbootApplication.class, args);
+	}
+
+}
