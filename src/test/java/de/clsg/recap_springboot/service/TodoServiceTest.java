cfdbc9a4f295652dd7bf85dab4cbfd23e0e04711
ChrisClsg <chris@clsg.de>
2026-03-20 16:18:50 +0100

    Create TodoEvents when adding, deleting or updating Todos and update HistoryState

--- src/test/java/de/clsg/recap_springboot/service/TodoServiceTest.java
+++ src/test/java/de/clsg/recap_springboot/service/TodoServiceTest.java
@@ -1,7 +1,9 @@
 package de.clsg.recap_springboot.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
@@ -15,13 +17,20 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.clsg.recap_springboot.dto.TodoDto;
+import de.clsg.recap_springboot.enums.TodoEventType;
 import de.clsg.recap_springboot.enums.TodoStatus;
+import de.clsg.recap_springboot.model.HistoryState;
 import de.clsg.recap_springboot.model.Todo;
+import de.clsg.recap_springboot.model.TodoEvent;
+import de.clsg.recap_springboot.repository.HistoryStateRepo;
+import de.clsg.recap_springboot.repository.TodoEventRepo;
 import de.clsg.recap_springboot.repository.TodoRepo;
 
 @ExtendWith(MockitoExtension.class)
 public class TodoServiceTest {
+  @Mock HistoryStateRepo historyStateRepo;
   @Mock IdService idService;
+  @Mock TodoEventRepo eventRepo;
   @Mock TodoRepo todoRepo;
   @InjectMocks private TodoService todoService;
 
@@ -59,7 +68,11 @@ public class TodoServiceTest {
   void addTodo_returnsSavedTodo_whenCalledWithValidDto() {
     TodoDto dto = validDto();
     String expectedId = "some-id";
+    int currentSequence = 1;
+    int expectedSequence = 2;
     when(idService.randomId()).thenReturn(expectedId);
+    when(historyStateRepo.findById("global")).thenReturn(Optional.of(new HistoryState("global", currentSequence)));
+
     Todo expectedTodo = Todo.builder()
       .description(dto.description())
       .status(dto.status())
@@ -67,9 +80,24 @@ public class TodoServiceTest {
       .build();
 
     assertEquals(expectedTodo, todoService.addTodo(dto));
-    verify(idService).randomId();
+    verify(idService, times(2)).randomId();
     verify(todoRepo).save(expectedTodo);
     verifyNoMoreInteractions(idService, todoRepo);
+
+    verify(eventRepo).save(new TodoEvent(
+      "some-id",
+      expectedSequence,
+      expectedTodo.id(),
+      new TodoDto(expectedTodo.description(), expectedTodo.status()),
+      null,
+      TodoEventType.CREATE
+    ));
+    verify(eventRepo).deleteBySequenceGreaterThan(currentSequence);
+    verifyNoMoreInteractions(eventRepo);
+
+    verify(historyStateRepo).findById("global");
+    verify(historyStateRepo).save(new HistoryState("global", expectedSequence));
+    verifyNoMoreInteractions(historyStateRepo);
   }
 
   @Test
@@ -80,6 +108,9 @@ public class TodoServiceTest {
     assertEquals(Optional.empty(), todoService.updateTodo("does not exist", dto));
     verify(todoRepo).findById("does not exist");
     verifyNoMoreInteractions(todoRepo);
+
+    verifyNoInteractions(eventRepo);
+    verifyNoInteractions(historyStateRepo);
   }
 
   @Test
@@ -89,12 +120,31 @@ public class TodoServiceTest {
     Todo expectedTodo = todo
       .withDescription(dto.description())
       .withStatus(dto.status());
+    int currentSequence = 1;
+    int expectedSequence = 2;
     when(todoRepo.findById(todo.id())).thenReturn(Optional.ofNullable(todo));
+    when(idService.randomId()).thenReturn("event-123");
+    when(historyStateRepo.findById("global")).thenReturn(Optional.of(new HistoryState("global", 1)));
 
     assertEquals(Optional.ofNullable(expectedTodo), todoService.updateTodo(todo.id(), dto));
     verify(todoRepo).findById(todo.id());
     verify(todoRepo).save(expectedTodo);
     verifyNoMoreInteractions(todoRepo);
+
+    verify(eventRepo).save(new TodoEvent(
+      "event-123",
+      expectedSequence,
+      todo.id(),
+      new TodoDto(expectedTodo.description(), expectedTodo.status()),
+      new TodoDto(todo.description(), todo.status()),
+      TodoEventType.UPDATE
+    ));
+    verify(eventRepo).deleteBySequenceGreaterThan(currentSequence);
+    verifyNoMoreInteractions(eventRepo);
+
+    verify(historyStateRepo).findById("global");
+    verify(historyStateRepo).save(new HistoryState("global", expectedSequence));
+    verifyNoMoreInteractions(historyStateRepo);
   }
 
   @Test
@@ -104,16 +154,39 @@ public class TodoServiceTest {
     assertFalse(todoService.deleteTodo("does not exist"));
     verify(todoRepo).findById("does not exist");
     verifyNoMoreInteractions(todoRepo);
+
+    verifyNoInteractions(eventRepo);
+    verifyNoInteractions(historyStateRepo);
   }
 
   @Test
   void deleteTodo_deletesTodoAndReturnsTrue_whenGivenExistingId() {
     Todo todo = validTodo();
-    when(todoRepo.findById(todo.id())).thenReturn(Optional.ofNullable(todo));
+    int currentSequence = 1;
+    int expectedSequence = 2;
+    when(todoRepo.findById(todo.id())).thenReturn(Optional.of(todo));
+    when(idService.randomId()).thenReturn("event-123");
+    when(historyStateRepo.findById("global")).thenReturn(Optional.of(new HistoryState("global", currentSequence)));
 
     assertTrue(todoService.deleteTodo(todo.id()));
+
     verify(todoRepo).findById(todo.id());
     verify(todoRepo).deleteById(todo.id());
     verifyNoMoreInteractions(todoRepo);
+
+    verify(eventRepo).save(new TodoEvent(
+      "event-123",
+      expectedSequence,
+      todo.id(),
+      null,
+      new TodoDto(todo.description(), todo.status()),
+      TodoEventType.DELETE
+    ));
+    verify(eventRepo).deleteBySequenceGreaterThan(currentSequence);
+    verifyNoMoreInteractions(eventRepo);
+
+    verify(historyStateRepo).findById("global");
+    verify(historyStateRepo).save(new HistoryState("global", expectedSequence));
+    verifyNoMoreInteractions(historyStateRepo);
   }
 }
